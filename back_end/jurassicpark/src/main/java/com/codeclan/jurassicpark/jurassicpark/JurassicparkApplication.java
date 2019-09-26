37595d4614703c692a624a656c301cf26aabd5bd
kat3005 <kasia.alicja@gmail.com>
2019-09-26 15:01:36 +0100

    Adds back-end framework

---
+++ back_end/jurassicpark/src/main/java/com/codeclan/jurassicpark/jurassicpark/JurassicparkApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.jurassicpark.jurassicpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurassicparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurassicparkApplication.class, args);
+	}
+
+}
