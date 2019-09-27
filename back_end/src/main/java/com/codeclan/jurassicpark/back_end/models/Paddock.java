6eea754dd7d87bd652a035d7fd26263cb323426d
gavinblack87 <gavin_black@hotmail.co.uk>
2019-09-27 15:55:28 +0100

    paddock class set-up

--- back_end/src/main/java/com/codeclan/jurassicpark/back_end/models/Paddock.java
+++ back_end/src/main/java/com/codeclan/jurassicpark/back_end/models/Paddock.java
@@ -1,4 +1,80 @@
 package com.codeclan.jurassicpark.back_end.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="paddocks")
 public class Paddock {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="visitor_capacity")
+    private int visitorCapacity;
+
+    @Column(name="dinosaur_capacity")
+    private int dinosaurCapacity;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "paddock", fetch = FetchType.LAZY)
+    private List<Dinosaur> dinosaurs;
+
+    public Paddock(String name, int visitorCapacity, int dinosaurCapacity) {
+        this.name = name;
+        this.visitorCapacity = visitorCapacity;
+        this.dinosaurCapacity = dinosaurCapacity;
+        this.dinosaurs = new ArrayList<>();
+
+    }
+
+    public Paddock() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVisitorCapacity() {
+        return visitorCapacity;
+    }
+
+    public void setVisitorCapacity(int visitorCapacity) {
+        this.visitorCapacity = visitorCapacity;
+    }
+
+    public int getDinosaurCapacity() {
+        return dinosaurCapacity;
+    }
+
+    public void setDinosaurCapacity(int dinosaurCapacity) {
+        this.dinosaurCapacity = dinosaurCapacity;
+    }
+
+    public List<Dinosaur> getDinosaurs() {
+        return dinosaurs;
+    }
+
+    public void setDinosaurs(List<Dinosaur> dinosaurs) {
+        this.dinosaurs = dinosaurs;
+    }
 }
--- back_end/src/main/java/com/codeclan/jurassicpark/back_end/models/Park.java
+++ back_end/src/main/java/com/codeclan/jurassicpark/back_end/models/Park.java
@@ -1,4 +1,77 @@
 package com.codeclan.jurassicpark.back_end.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="park")
 public class Park {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "capacity")
+    private int capacity;
+
+    @Column(name = "lockdown")
+    private Boolean lockdown;
+
+    @Column(name="paddock_capacity")
+    private int paddockCapacity;
+
+
+
+    public Park(String name, int capacity, Boolean lockdown, int paddockCapacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.lockdown = lockdown;
+        this.paddockCapacity = paddockCapacity;
+    }
+
+    public Park() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Boolean getLockdown() {
+        return lockdown;
+    }
+
+    public void setLockdown(Boolean lockdown) {
+        this.lockdown = lockdown;
+    }
+
+    public int getPaddockCapacity() {
+        return paddockCapacity;
+    }
+
+    public void setPaddockCapacity(int paddockCapacity) {
+        this.paddockCapacity = paddockCapacity;
+    }
 }
