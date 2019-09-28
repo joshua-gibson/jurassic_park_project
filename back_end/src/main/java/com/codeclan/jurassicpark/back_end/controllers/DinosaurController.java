8465d767fe7f2ee01f472c8a0b60e95d97f2d67f
gavinblack87 <gavin_black@hotmail.co.uk>
2019-09-28 14:07:26 +0100

    first query

--- back_end/src/main/java/com/codeclan/jurassicpark/back_end/controllers/DinosaurController.java
+++ back_end/src/main/java/com/codeclan/jurassicpark/back_end/controllers/DinosaurController.java
@@ -1,4 +1,21 @@
 package com.codeclan.jurassicpark.back_end.controllers;
 
+import com.codeclan.jurassicpark.back_end.models.Dinosaur;
+import com.codeclan.jurassicpark.back_end.repositories.DinosaurRepository.DinosaurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 public class DinosaurController {
+
+    @Autowired
+    DinosaurRepository dinosaurRepository;
+
+    @GetMapping(value = "/park/{id}")
+    public List<Dinosaur> findByPark(@PathVariable Long id){
+        return dinosaurRepository.findByPaddocksParkId(id);
+    }
+
 }
--- back_end/src/main/java/com/codeclan/jurassicpark/back_end/repositories/DinosaurRepository/DinosaurRepositoryCustom.java
+++ back_end/src/main/java/com/codeclan/jurassicpark/back_end/repositories/DinosaurRepository/DinosaurRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.codeclan.jurassicpark.back_end.repositories.DinosaurRepository;
 
+import com.codeclan.jurassicpark.back_end.models.Dinosaur;
+
+import java.util.List;
+
 public interface DinosaurRepositoryCustom {
+
 }
