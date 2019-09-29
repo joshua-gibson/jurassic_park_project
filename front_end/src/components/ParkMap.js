51166eee3a40ba2d201b461dd2dd9bcb587e8b3f
joshua-gibson <flash50k@gmail.com>
2019-09-29 21:26:15 +0100

    fancier paddocks

--- front_end/src/components/ParkMap.js
+++ front_end/src/components/ParkMap.js
@@ -8,16 +8,16 @@ const ParkMap = () => {
        <div className="maparea">
 
        <a href="/paddocks">
-        <div className="paddock p1">p1 </div>
+        <div className="paddock p1">p1 <span class="tooltiptext">Paddock Name</span> </div>
         </a>
         <a href="/paddocks">
-        <div className="paddock p2"> p2 </div>
+        <div className="paddock p2"> p2 <span class="tooltiptext">Paddock Name</span></div>
         </a>
         <a href="/paddocks">
-        <div className="paddock p3"> p3 </div>
+        <div className="paddock p3"> p3 <span class="tooltiptext">Paddock Name</span></div>
         </a>
         <a href="/paddocks">
-        <div className="paddock p4"> p4 </div>
+        <div className="paddock p4"> p4 <span class="tooltiptext">Paddock Name</span></div>
         </a>
 
 
