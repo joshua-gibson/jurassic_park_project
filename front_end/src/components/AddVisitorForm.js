a0821f234c1cb37126b97914b00d403a9e86f76e
mikefallon <mikefallon81@gmail.com>
2019-09-29 14:09:01 +0100

    visitor form added and updated int showing in component on dev tools

--- front_end/src/components/AddVisitorForm.js
+++ front_end/src/components/AddVisitorForm.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const AddVisitorForm = () => {
+class AddVisitorForm extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      visitors: 0
+    }
+    this.handleVisitorChange = this.handleVisitorChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    const newVisitors = {
+      visitors: this.state.salary
+    }
+    this.props.handleNewVisitors(newVisitors);
+  }
+
+  handleVisitorChange(event){
+    this.setState({visitors: event.target.value});
+  }
+
+render(){
   return (
-    <>
-       <h1>AddVisitorForm placeholder</h1>
-    </>
-  );
+    <div>
+       <h1>AddVisitorForm</h1>
+       <form onSubmit={this.handleSubmit}>
+       <label htmlFor="visitors">No Of Visitors</label>
+       <input id="visitors"
+       value={this.state.visitors}
+       type="number"
+       onChange={this.handleVisitorChange}/>
+       <input type="submit" />
+       </form>
+    </div>
+  )};
 
   }
 
