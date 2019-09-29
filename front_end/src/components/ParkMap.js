642b6443150063a954620a72eb14cb9dcc8af545
joshua-gibson <flash50k@gmail.com>
2019-09-29 21:33:01 +0100

    tweaks

--- front_end/src/components/ParkMap.js
+++ front_end/src/components/ParkMap.js
@@ -8,16 +8,19 @@ const ParkMap = () => {
        <div className="maparea">
 
        <a href="/paddocks">
-        <div className="paddock p1">p1 <span class="tooltiptext">Paddock Name</span> </div>
+        <div className="paddock p1"> p1 <span class="tooltiptext">Paddock P1 Name</span> </div>
         </a>
         <a href="/paddocks">
-        <div className="paddock p2"> p2 <span class="tooltiptext">Paddock Name</span></div>
+        <div className="paddock p2"> p2 <span class="tooltiptext">Paddock P2 Name</span></div>
         </a>
         <a href="/paddocks">
-        <div className="paddock p3"> p3 <span class="tooltiptext">Paddock Name</span></div>
+        <div className="paddock p3"> p3 <span class="tooltiptext">Paddock P3 Name</span></div>
         </a>
         <a href="/paddocks">
-        <div className="paddock p4"> p4 <span class="tooltiptext">Paddock Name</span></div>
+        <div className="paddock p4"> p4 <span class="tooltiptext">Paddock P4 Name</span></div>
+        </a>
+        <a href="/paddocks">
+        <div className="paddock p5"> p5 <span class="tooltiptext">Paddock P5 Name</span></div>
         </a>
 
 
