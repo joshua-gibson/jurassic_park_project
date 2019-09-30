249bc58d829b121b833ac5d2ae4b27a3a9606a68
Joshua Gibson <flash50k@gmail.com>
2019-09-30 14:48:32 +0100

    tweak

--- front_end/src/components/DinoCardList.js
+++ front_end/src/components/DinoCardList.js
@@ -6,7 +6,7 @@ import '../style/card.css'
 const DinoCardList = (props) => {
 
    const dinos = props.dinosaurs.map((dinosaur, index) => {
-     return <DinosaurCard key={index} name={dinosaur.name} avatar={"C"} subheader={dinosaur.species.dietType} image={dinosaur.species.url} content={dinosaur.species.info}/>
+     return <DinosaurCard key={index} name={dinosaur.name} avatar={dinosaur.species.dietType.charAt(0)} subheader={dinosaur.species.dietType} image={dinosaur.species.url} content={dinosaur.species.info}/>
 
      
     //  <p key={index}> {dinosaur.name}</p>
