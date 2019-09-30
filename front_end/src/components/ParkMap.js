b3fc3bcf493a1cab24f4d35d44be8f357ed16464
Joshua Gibson <flash50k@gmail.com>
2019-09-30 11:37:30 +0100

    park map paddocks are now dynamic

--- front_end/src/components/ParkMap.js
+++ front_end/src/components/ParkMap.js
@@ -3,26 +3,17 @@ import '../style/ParkMap.css';
 
 const ParkMap = (props) => {
 
+  const displayedPaddocks = props.paddocks.map((paddock, index) => {
+    return  <a href="/paddocks" key={index}>
+            <div className={`paddock p${index}`}> p{index} <span className="tooltiptext">{paddock.name}</span> </div>
+            </a>
+  })
+
   return (
     <>
        <div className={"maparea "  + (props.lockdown ? "lockdown" : "maparea") }>
 
-       <a href="/paddocks">
-        <div className="paddock p1"> p1 <span className="tooltiptext">Paddock P1 Name</span> </div>
-        </a>
-        <a href="/paddocks">
-        <div className="paddock p2"> p2 <span className="tooltiptext">Paddock P2 Name</span></div>
-        </a>
-        <a href="/paddocks">
-        <div className="paddock p3"> p3 <span className="tooltiptext">Paddock P3 Name</span></div>
-        </a>
-        <a href="/paddocks">
-        <div className="paddock p4"> p4 <span className="tooltiptext">Paddock P4 Name</span></div>
-        </a>
-        <a href="/paddocks">
-        <div className="paddock p5"> p5 <span className="tooltiptext">Paddock P5 Name</span></div>
-        </a>
-
+          {displayedPaddocks}
 
         <img className="parkmap" src='./images/ParkBackground.png' alt="Park Landscape"></img>
        </div>
